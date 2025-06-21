65d260000badf1427c89729ecf28f76fd8c4c506
sivapothabathuni <kumarramlucky98@gmail.com>
2025-06-21 14:08:44 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -11,5 +11,16 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        # Process form data here
+        return redirect(url_for('index'))
+    return render_template('contact.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
